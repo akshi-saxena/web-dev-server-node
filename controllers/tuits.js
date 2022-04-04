18e3df2fd40a005ab8c8c4c287c6cbb269903815
Akshi Saxena <akshi305.as@gmail.com>
2022-04-04 15:49:18 -0400

    a8 REST complete

---
+++ controllers/tuits.js
@@ -0,0 +1,21 @@
+export default [
+    {
+      "_id": "123",
+      "postedBy": {"username": "NASA"},
+      "tuit": "In 2021, our @NASAPersevere Mars rover landed",
+      "handle": 'NASA',  
+      "time": '2h',    
+      "likes": 345,
+      "dislikes": 0
+    },
+    {
+      "_id": "234",
+      "postedBy": { "username": "NASA" },
+      "tuit": "Ingenuity helicopter takes flight.",
+      "handle": 'NASA', 
+      "time": '5h',
+      "likes": 346785,
+      "dislikes": 0
+    }
+  ];
+  
