2188654bdcf713d8af00ef0eba381f189f10170c
Akshi Saxena <akshi305.as@gmail.com>
2022-04-04 15:50:13 -0400

    a8 REST complete

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from 'express';
-import helloController
-  from "./controllers/hello-controller.js";
+import helloController from "./controllers/hello-controller.js";
 import userController from './controllers/user-controller.js';
 import tuitsController from './controllers/tuits-controller.js';
 import cors from 'cors';
