9fa639c10c3059f99732c99f8682b2c9fa4020ba
Akshi Saxena <akshi305.as@gmail.com>
2022-04-05 12:31:27 -0400

    process env setup

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import userController from './controllers/user-controller.js';
 import tuitsController from './controllers/tuits-controller.js';
 import cors from 'cors';
 
-const cors_options = {origin : "http://localhost:3000"};
+const cors_options = {credentials: true, origin : "http://localhost:3000"};
 
 const app = express();
 app.use(cors(cors_options));
