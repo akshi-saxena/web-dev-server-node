c5a5d2c7c84270b7e95c69cb45528c623080157c
Akshi Saxena <akshi305.as@gmail.com>
2022-04-11 19:22:42 -0400

    a9 node

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 // import tuitsController from "./tuits/tuits-controller.js";
 
-mongoose.connect('mongodb+srv://akshisaxena:pFzclIktUtnzYcwr@cluster0.jvygh.mongodb.net/webdev?retryWrites=true&w=majority');
-
-// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/webdev'
-// mongoose.connect(CONNECTION_STRING);
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/webdev'
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors());
